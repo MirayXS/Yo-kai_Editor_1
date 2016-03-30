430c64ada48e55c081e92fda444f7f5258791592
togenyan <nyantoge@gmail.com>
2016-03-30 15:43:20 +0900

    use system default locale

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QLocale::setDefault(QLocale(QLocale::English, QLocale::UnitedStates));
-    QTranslator qtTranslator;
-    qtTranslator.load("qt_" + QLocale().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    a.installTranslator(&qtTranslator);
     MainWindow w;
     w.show();
 
